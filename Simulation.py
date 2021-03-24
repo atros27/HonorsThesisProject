746ff20d8f72ef5b24f10fba6e95cae8ec92a1f0
Andrew Trostle <atros27@utexas.edu>
2021-03-24 09:33:15 -0500

    Wind Model Linked to Simulation and Fixed Aerodynamics Function. Also, preliminary stress calculations.

--- Simulation.py
+++ Simulation.py
@@ -3,11 +3,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy as sp
 from scipy.integrate import solve_ivp
+import time
 
 #TESTSIM3 : Biaxial Bending-only Rayleigh-Ritz 2DOF ODE
 
 class Simulation:
-    def __init__(self):
+    def __init__(self, wind):
+        #Todo: Align primary wind direction with tip bodies
         self.E = 68e6 #Aluminum Young's Modulus Todo: Replace with Material Class that holds these constants
         self.G = 25e6 #Aluminum Shear Modulus
         self.rho = 2.71e3 #kg/m^3
@@ -20,8 +22,9 @@ class Simulation:
         self.e = .1
         self.L = .25 #Note: Tip bodies and shaft-beam have identical length
 
-        self.U = 10
-        self.delta = 120/180*math.pi
+        #self.U = 10
+        #self.delta = 120/180*math.pi
+        self.wind = wind
 
         self.J = 0.5*math.pi*math.pow(self.r, 4)
         self.I = 0.25*math.pi*math.pow(self.r, 4)
@@ -70,8 +73,24 @@ class Simulation:
         gamma_0 = np.array([0, 0, 0, 0, .01, .01, .01, .01])
         #dt = .01
         #t = np.arange(.1, step=dt)
-        t = (0, 30)
-        solution = solve_ivp(self.derivative, t, gamma_0, full_output=1, mxstep=1 )
+        t = (0, 5)
+        start = time.time()
+        time_series = np.arange(0, 301)
+        discrete_wind_series = np.array([self.wind.update() for i in range(0,301)])
+        self.speed_series = sp.interpolate.interp1d(time_series, discrete_wind_series[:,0])
+        self.direction_series = sp.interpolate.interp1d(time_series, discrete_wind_series[:,1])
+        plt.figure()
+        plt.plot(time_series, discrete_wind_series[:,0], 'b', label="Synthetic Speed")
+        plt.legend()
+        plt.figure()
+        plt.plot(time_series, discrete_wind_series[:,1], 'b', label="Synthetic Direction")
+        plt.legend()
+        end = time.time()
+        print("Synthetic Wind Series Created. Time Elapsed:",math.floor(end-start),"seconds")
+        start = time.time()
+        solution = solve_ivp(self.derivative, t, gamma_0, full_output=1, method='RK23')
+        end = time.time()
+        print("IVP Solved. Time Elapsed:",math.floor(end-start),"seconds")
         #print("HU: ",info.get('hu'))
         #print("NST: ",info.get('nst'))
         #print("NFE: ",info.get('nfe'))
@@ -98,6 +117,20 @@ class Simulation:
         w_x = solution.y[0, :]+solution.y[1, :]
         w_y = solution.y[2, :]+solution.y[3, :]
 
+        tip_moment_x = self.E*self.I*((2/self.L/self.L)*solution.y[0, :] + (6/self.L/self.L)*solution.y[1, :])
+        tip_moment_y = self.E*self.I*((2/self.L/self.L)*solution.y[2, :] + (6/self.L/self.L)*solution.y[3, :])
+        tip_shear_x = self.E*self.I*(6/self.L/self.L/self.L)*solution.y[1, :]
+        tip_shear_y = self.E*self.I*(6/self.L/self.L/self.L)*solution.y[3, :]
+        tip_max_bending_stress_x = self.r/self.I*tip_moment_x
+        tip_max_bending_stress_y = self.r/self.I*tip_moment_y
+
+        root_moment_x = self.E*self.I*(2/self.L/self.L)*solution.y[0, :]
+        root_moment_y = self.E*self.I*(2/self.L/self.L)*solution.y[2, :]
+        root_shear_x = tip_shear_x
+        root_shear_y = tip_shear_y
+        root_max_bending_stress_x = self.r/self.I*root_moment_x
+        root_max_bending_stress_y = self.r/self.I*root_moment_y
+
         #print(solution[0:100, 0])
         #print(solution[0:100, 1])
         #print(theta)
@@ -108,20 +141,44 @@ class Simulation:
 
         #plt.plot(solution.t, theta, 'b', label="theta")
         #plt.plot(t, theta_prime, 'g', label="theta_prime")
+        plt.figure()
         plt.plot(solution.t, w_x, 'g', label="w_x")
         plt.plot(solution.t, w_y, 'r', label="w_y")
+        plt.legend()
+        plt.figure()
+        plt.plot(solution.t, tip_max_bending_stress_x, 'g', label="tip_bending_stress_x")
+        plt.plot(solution.t, tip_max_bending_stress_y, 'r', label="tip_bending_stress_y")
+        plt.legend()
+        plt.figure()
+        plt.plot(solution.t, root_max_bending_stress_x, 'g', label="root_bending_stress_x")
+        plt.plot(solution.t, root_max_bending_stress_y, 'r', label="root_bending_stress_y")
+        plt.legend()
+        plt.figure()
+        plt.plot(solution.t, tip_shear_x, 'g', label="tip_shear_x")
+        plt.plot(solution.t, tip_shear_y, 'r', label="tip_shear_y")
+        plt.legend()
+        plt.figure()
+        plt.plot(solution.t, root_shear_x, 'g', label="root_shear_x")
+        plt.plot(solution.t, root_shear_y, 'r', label="root_shear_y")
+        plt.legend()
         print("Iterations: ",self.iterations)
 
     def derivative(self, t, gamma): #dy/dt function for odeint
         self.iterations += 1
         #print("Time: ",t)
-        [Fx, Fy] = self.aerodynamics(gamma)
-        print(Fx, Fy)
+        [Fx, Fy] = self.aerodynamics(t, gamma)
+        #print(Fx, Fy)
         d_gamma_dt = self.derivative_matrix.dot(gamma) + self.derivative_vector_x.dot(Fx) + self.derivative_vector_y.dot(Fy)
         #print(d_gamma_dt)
         return d_gamma_dt
 
-    def aerodynamics(self, gamma):
+    def aerodynamics(self, t, gamma):
+        #U = self.speed_series(t)
+        #delta = self.direction_series(t)
+        #Static Wind Test Case
+        U = 5
+        delta = 120
+
         #theta = gamma[0] + gamma[1]
         #theta = (theta+2*math.pi) % (2*math.pi)
         #print("Theta: ",theta)
@@ -130,30 +187,30 @@ class Simulation:
         #theta_dot = gamma[6] + gamma[7]
         x_dot = gamma[4] + gamma[5]
         y_dot = gamma[6] + gamma[7]
-        print("X_Dot: ", x_dot)
-        print("Y_Dot: ", y_dot)
+        #print("X_Dot: ", x_dot)
+        #print("Y_Dot: ", y_dot)
         #print("Theta_Dot: ", theta_dot)
 
         x1_dot = x_dot
         x2_dot = x_dot
         y1_dot = y_dot
         y2_dot = y_dot
-        print("1: ",x1_dot, y1_dot)
-        print("2: ",x2_dot, y2_dot)
+        #print("1: ",x1_dot, y1_dot)
+        #print("2: ",x2_dot, y2_dot)
 
-        v_r1_2 = math.pow(self.U*math.cos(self.delta) - x1_dot, 2) + math.pow(self.U*math.sin(self.delta) - y1_dot, 2)
-        v_r2_2 = math.pow(self.U*math.cos(self.delta) - x2_dot, 2) + math.pow(self.U*math.sin(self.delta) - y2_dot, 2)
+        v_r1_2 = math.pow(U*math.cos(delta) - x1_dot, 2) + math.pow(U*math.sin(delta) + y1_dot, 2)
+        v_r2_2 = math.pow(U*math.cos(delta) - x2_dot, 2) + math.pow(U*math.sin(delta) + y2_dot, 2)
 
         #alpha_1 = math.pi/3 - theta + math.atan2(self.U*math.sin(self.delta) - y1_dot, self.U*math.cos(self.delta) - x1_dot)
-        alpha_1 = math.pi / 3 + math.atan2(self.U*math.sin(self.delta) - y1_dot, self.U*math.cos(self.delta) - x1_dot)
+        alpha_1 = math.pi / 3 + math.atan2(U*math.sin(delta) + y1_dot, U*math.cos(delta) - x1_dot)
         #alpha_1 = (alpha_1+2*math.pi) % (2*math.pi)
-        alpha_2 = math.pi/3 + math.atan2(self.U*math.sin(self.delta) - y2_dot, self.U * math.cos(self.delta) - x2_dot)
-        print("Alpha: ",alpha_1, alpha_2)
+        alpha_2 = math.pi/3 + math.atan2(U*math.sin(delta) + y2_dot, U * math.cos(delta) - x2_dot)
+        #print("Alpha: ",alpha_1, alpha_2)
 
-        F_x1 = -.5*self.air_density*v_r1_2 * self.b*self.L * ( -self.c_l(alpha_1)*math.sin(alpha_1-math.pi/3) + self.c_d(alpha_1)*math.cos(alpha_1-math.pi/3) )
-        F_y1 = -.5*self.air_density*v_r1_2 * self.b*self.L * ( self.c_l(alpha_1)*math.cos(alpha_1-math.pi/3) + self.c_d(alpha_1)*math.sin(alpha_1-math.pi/3) )
-        F_x2 = -.5*self.air_density*v_r2_2 * self.b*self.L * ( -self.c_l(alpha_2)*math.sin(alpha_2-math.pi/3) + self.c_d(alpha_2)*math.cos(alpha_2-math.pi/3) )
-        F_y2 = -.5*self.air_density*v_r2_2 * self.b*self.L * ( self.c_l(alpha_2)*math.cos(alpha_2-math.pi/3) + self.c_d(alpha_2)*math.sin(alpha_2-math.pi/3) )
+        F_x1 = .5*self.air_density*v_r1_2 * self.b*self.L * ( -self.c_l(alpha_1)*math.sin(alpha_1-math.pi/3) + self.c_d(alpha_1)*math.cos(alpha_1-math.pi/3) )
+        F_y1 = .5*self.air_density*v_r1_2 * self.b*self.L * ( self.c_l(alpha_1)*math.cos(alpha_1-math.pi/3) + self.c_d(alpha_1)*math.sin(alpha_1-math.pi/3) )
+        F_x2 = .5*self.air_density*v_r2_2 * self.b*self.L * ( -self.c_l(alpha_2)*math.sin(alpha_2-math.pi/3) + self.c_d(alpha_2)*math.cos(alpha_2-math.pi/3) )
+        F_y2 = .5*self.air_density*v_r2_2 * self.b*self.L * ( self.c_l(alpha_2)*math.cos(alpha_2-math.pi/3) + self.c_d(alpha_2)*math.sin(alpha_2-math.pi/3) )
 
 
         #F_x2 = 0
--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-from OldSimulation import Simulation
+import math
+from Simulation import Simulation
 from TestSim1 import TestSim1
 from TestSim2 import TestSim2
 from TestSim3 import TestSim3
+import time
 from WindModel import WindModel
 
 import matplotlib
 
 def __main__():
-    #WindModel_1 = WindModel('C:/Users/atros27/Documents/WindData2.xlsx')
-    #Simulation_1 = Simulation()
-    TestSim2_1 = TestSim2()
+    start = time.time()
+    WindModel_1 = WindModel('C:/Users/atros27/Documents/WindData2.xlsx')
+    end = time.time()
+    print("Wind Model Created. Time Elapsed:",math.floor(end-start),"seconds")
+    Simulation_1 = Simulation(WindModel_1)
+    #TestSim2_1 = TestSim2()
     #print(WindModel_1.results.summary())
     #figure = WindModel_1.results.plot_forecast(10000)
     matplotlib.pyplot.show()
